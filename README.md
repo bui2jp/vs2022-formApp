09442f69c700bd6c11ed5164826c0b07dcb7784a
chikutaku <chikutaku.reiwa.2020@gmail.com>
2025-02-06 16:00:36 +0900

    add: Material Design in Xaml

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 ﻿# vs2022-formApp
 
-windowsでの開発環境
+windowsでの開発環境には Visual Studio 2022 Community Edition を使用することにする。
 
-git の設定
 
-aa
+# フォームアプリケーション
+
+```
+Windows Formアプリケーション(.NET 4.8) (これはもう古い)
+Windows Formアプリケーション(.NET 8)
+WPFアプリケーション(.NET 8)
+```
+基本的には .NET 8 を使用する。昔の.NET Coreが .NET 5以降統一されている。
+現在の最新
+
+| version | サポート |
+| --- | --- |
+|.NET 9.0.1| 標準期間サポート (STS) 18 か月|
+|.NET 8.0.1| 長期的なサポート (LTS) 3年間|
+
+# NuGetパッケージ
+
+.NETプラットフォームのためのパッケージマネージャー
+
+Material Design In XAML Toolkitを試してみる。
--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "これは .net 4.8 古いタイプ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
